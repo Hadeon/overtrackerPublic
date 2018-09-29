6128e57de886852cc8fe4208944787ffff84dd65
Hadeon <andersene909@gmail.com>
2018-09-29 15:43:19 -0700

    Updating NPM scripts; Minifying client code

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Routes from './pages/Routes';
+
+import Home from './pages/Home'
+import About from './pages/About'
 
 class App extends Component {
   render() {
     return (
-      <Routes/>
+      <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <hr />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </div>
+    </Router>
     );
   }
 }
