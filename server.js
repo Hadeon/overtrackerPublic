1f0104672a5632cc6d3b308e5125eb58e9b0c8e1
Hadeon <andersene909@gmail.com>
2019-06-04 15:20:18 -0700

    Remove keys requirement

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const passport = require('passport')
 
 const app = express()
-const keys = require('./config/keys')
+// const keys = require('./config/keys')
 const dotenv = require('dotenv');
 dotenv.config();
 
